ccf8b36622993c5c0ff41542eb89ea8a69ed417f
Inioluwa Oyetunji <theinioye@gmail.com>
2024-08-23 05:24:56 +0100

    Aug 22 commit

--- compile.ts
+++ compile.ts
@@ -1,6 +1,17 @@
-import {Question} from "./readline"
+import { createUser } from "./createUser";
+import readlineSync from "readline-sync";
 
+async function main() {
+  const firstname = readlineSync
+    .question(`Please provide the author's first name here →`)
+    .trim();
+  const lastname = readlineSync
+    .question(`Please provide author's last name here ➡️`)
+    .trim();
+  console.log(`Thank you. Author's name will now be logged.`);
 
+  const authorName = await createUser(firstname, lastname);
 
-
-
+  console.log(authorName);
+}
+main();
--- question.ts
+++ question.ts
@@ -1,26 +1,9 @@
 import { PrismaClient } from "@prisma/client";
+export const prisma = new PrismaClient();
 
 
-export async function main (){
-    const author = await prisma.author.create({
-        data: {
-            firstname : ,
-            lastname  :
-        }
-    })
-    console.log (author)
-}
 
-main()
 
-.then(async () => {
-    await prisma.$disconnect()
-})
-.catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-})
 
 
 
